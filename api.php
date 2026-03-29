387cf4414d8b839dfa90c564bdabbc23e5e6e002
Jems-maker <jemminguito13@gmail.com>
2026-03-29 21:43:06 +0800

    Migrate download counter to API & remove top install banner

--- api.php
+++
@@ -1,5 +0,0 @@
-<?php
-$file = 'downloads.txt';
-$count = file_exists($file) ? (int)file_get_contents($file) : 0;
-echo $count;
-?>
--- download.php
+++
@@ -1,8 +0,0 @@
-<?php
-$file = 'downloads.txt';
-$count = file_exists($file) ? (int)file_get_contents($file) : 0;
-$count++;
-file_put_contents($file, $count);
-header('Location: pet-feeder-app-release.zip');
-exit;
-?>
